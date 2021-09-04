25499f2f8fe461f738d029d7f2d64ee25e7e75fb
businan <yorulmazburaksinan@gmail.com>
2021-09-04 22:45:11 +0200

    add typograph

--- src/components/material-ui/index.js
+++ src/components/material-ui/index.js
@@ -1,18 +1,45 @@
-import React from 'react'
-import Buttons from './buttons/Buttons'
-import CheckBoxComp from './checkBox/CheckBoxComp'
-import StyledButton from './styled-button/StyledButton'
-import TextFieldComp from './text-field/TextFieldComp'
+import React from "react";
+import Buttons from "./buttons/Buttons";
+import CheckBoxComp from "./checkBox/CheckBoxComp";
+import StyledButton from "./styled-button/StyledButton";
+import TextFieldComp from "./text-field/TextFieldComp";
+
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
+import { orange, green } from "@material-ui/core/colors";
+import TypographyComp from "./typography/TypographyComp";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: orange[400],
+        },
+        secondary: {
+            main: green[500],
+        },
+    },
+    typography: {
+        h2: {
+            fontSize: 38,
+            marginBottom: 10,
+        },
+        subtitle1: {
+            marginBottom: 10,
+        },
+    },
+});
 
 const MaterialUIComponents = () => {
     return (
         <>
-            <StyledButton />
-            <TextFieldComp /> 
-            <CheckBoxComp />
-            <Buttons />
+            <ThemeProvider theme={theme}>
+                <TypographyComp />
+                <StyledButton />
+                <TextFieldComp />
+                <Buttons />
+                <CheckBoxComp />
+            </ThemeProvider>
         </>
-    )
-}
+    );
+};
 
-export default MaterialUIComponents
+export default MaterialUIComponents;
---
+++ src/components/material-ui/typography/TypographyComp.js
@@ -0,0 +1,16 @@
+import Typography from "@material-ui/core/Typography"
+
+const TypographyComp = () => {
+    return (
+        <>
+            <Typography variant='h2' component="div">
+                Clarusway MUI Session
+            </Typography>
+            <Typography variant='subtitle1' component="div">
+                We are learning MUI
+            </Typography>
+        </>
+    )
+}
+
+export default TypographyComp
